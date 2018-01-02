6a5a1119dabade06f8cc1965cca6ef9b87e96e3f
cool1 <cool1@DESKTOP-A07977D.fritz.box>
2018-01-02 21:13:51 +0100

    First Node Entities prepared for GraphDatabase.

--- IPC/src/main/java/PAYSURA/IPC/App.java
+++ IPC/src/main/java/PAYSURA/IPC/App.java
@@ -12,7 +12,7 @@ public class App {
     String home() {
         return "Hello World!";
     }
-
+    
     public static void main(String[] args) throws Exception {
         SpringApplication.run(App.class, args);
     }
