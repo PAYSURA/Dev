c42ac7fc6c4e2c12de9b086e8580254e13bb65bc
Kratersk88 <a.baklizki@gmx.de>
2018-01-16 19:54:08 +0100

    Secure Controller updated

--- com.paysura.service.backend/src/main/java/com/paysura/controller/secure/SecureController.java
+++ com.paysura.service.backend/src/main/java/com/paysura/controller/secure/SecureController.java
@@ -3,7 +3,9 @@
  */
 package com.paysura.controller.secure;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,8 +24,8 @@ import lombok.Data;
 public class SecureController {
 
 	@RequestMapping(method = RequestMethod.GET, value = "/", produces = MediaType.APPLICATION_JSON_UTF8_VALUE, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
-	public String index() {
-		return "Greetings from Spring Boot!";
+	public ResponseEntity<String> index() {
+		return new ResponseEntity<String>("Hello from Secure Controller", HttpStatus.OK);
 	}
 
 }
